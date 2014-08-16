06c6b1544bcad15423bd6a189af7f59c6c801f4a
Rance Horton <username@users.noreply.github.com>
2014-08-16 19:16:42 -0400

    Use utils in selection sort

--- src/problems/MySelectionSort.java
+++ src/problems/MySelectionSort.java
@@ -14,7 +14,7 @@ public class MySelectionSort {
 
 		for (int j = 0; j < length; j++) {
 			minValue = values.get(j);
-			for (int i = j+1; i < length; i++) {
+			for (int i = j + 1; i < length; i++) {
 				currValue = values.get(i);
 				comparisonResult = minValue.compareTo(currValue);
 				if (comparisonResult == 0) {
@@ -46,12 +46,9 @@ public class MySelectionSort {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int[] values = new In(args[0]).readAllInts();
-		AbstractList<Integer> vectorValues = new Vector<Integer>();
-		for (int i = 0; i < values.length; i++) {
-			vectorValues.add(values[i]);
-		}
-		
+
+		AbstractList<Integer> vectorValues = MyUtils.getVectorData(args);
+
 		MySelectionSort.sort(vectorValues);
 		System.out.println(vectorValues.toString());
 	}
